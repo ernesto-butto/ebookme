c55a883fe88a6863ca1214e7ef544215166ba191
poolebu <poolebu@gmail.com>
2016-01-08 14:47:44 -0500

    this works for the first time, but only the service of ebookglue

--- src/main/java/com/catwizard/service/DotepubRequest.java
+++ src/main/java/com/catwizard/service/DotepubRequest.java
@@ -1,53 +1,115 @@
 package com.catwizard.service;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLConnection;
+import java.util.Scanner;
 
 /**
  * Created by poolebu on 1/7/16.
  */
 public class DotEpubRequest {
 
-    public String sendRequest() throws Exception{
+    final String username = "ebookme2@gmail.com";
+    final String password = "SyVav5UB";
 
-        String urlToConvert = "http://jessewarden.com/2008/11/agile-chronicles-1-stressful.html";
+
+    public String getHtmlContent(String targetUrl){
+
+        String content = null;
+
+        URLConnection connection = null;
+
+        try {
+
+            connection =  new URL(targetUrl).openConnection();
+            Scanner scanner = new Scanner(connection.getInputStream());
+            scanner.useDelimiter("\\Z");
+            content = scanner.next();
+
+        }catch ( Exception ex ) {
+
+            ex.printStackTrace();
+
+        }
+
+        return content;
+
+    }
+
+    public File sendRequestForConversionDotEpub(String htmlToConvert, String originalSiteUrl, String format) throws Exception{
+
         String dotEpubUrl = "http://dotepub.com/v1/post";
+        String title = "TestTitle";
+        String fileName="/Users/poolebu/Desktop/book.mobi";
 
 
-        URL obj = new URL(dotEpubUrl);
-        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        URL url = new URL(dotEpubUrl);
+        HttpURLConnection openConnection = (HttpURLConnection) url.openConnection();
 
-        con.setRequestMethod("POST");
+        openConnection.setRequestMethod("POST");
 
-        String urlParameters = "html=xxx&title=xxx&urlxxx=&format=xxx&num=12345";
+        String urlParameters = "html="+htmlToConvert+
+                "&title="+originalSiteUrl+
+                "&url="+originalSiteUrl+
+                "&format="+format;
 
         // Send post request
-        con.setDoOutput(true);
-        DataOutputStream wr = new DataOutputStream(con.getOutputStream());
-        wr.writeBytes(urlParameters);
-        wr.flush();
-        wr.close();
+        openConnection.setDoOutput(true);
+        DataOutputStream dataOutputStream = new DataOutputStream(openConnection.getOutputStream());
+        dataOutputStream.writeBytes(urlParameters);
+        dataOutputStream.flush();
+        dataOutputStream.close();
 
-        int responseCode = con.getResponseCode();
+        int responseCode = openConnection.getResponseCode();
         System.out.println("\nSending 'POST' request to URL : " + dotEpubUrl);
         System.out.println("Post parameters : " + urlParameters);
         System.out.println("Response Code : " + responseCode);
 
-        BufferedReader in = new BufferedReader(
-                new InputStreamReader(con.getInputStream()));
-        String inputLine;
-        StringBuffer response = new StringBuffer();
 
-        while ((inputLine = in.readLine()) != null) {
-            response.append(inputLine);
+        // Dealing with the response
+        InputStream inputStream = null;
+        FileOutputStream outputStream = null;
+
+        try {
+
+            inputStream = openConnection.getInputStream();
+
+            outputStream = new FileOutputStream(fileName);
+
+            int read = 0;
+            byte[] bytes = new byte[1024];
+
+            while ((read = inputStream.read(bytes)) != -1) {
+                outputStream.write(bytes, 0, read);
+            }
+
+            System.out.println("Done!");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (outputStream != null) {
+                try {
+                    // outputStream.flush();
+                    outputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
         }
-        in.close();
 
 
-        return response.toString();
+        return new File(fileName);
 
 
     }
--- src/test/java/com/catwizard/service/DotEpubRequestTest.java
+++ src/test/java/com/catwizard/service/DotEpubRequestTest.java
@@ -2,11 +2,14 @@ package com.catwizard.service;
 
 import com.catwizard.MainApplication;
 import junit.framework.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+
+import java.io.File;
 
 /**
  * Created by poolebu on 1/7/16.
@@ -16,20 +19,73 @@ import org.springframework.test.context.web.WebAppConfiguration;
 @WebAppConfiguration
 public class DotEpubRequestTest {
 
+    String urlToConvert = "http://jessewarden.com/2008/11/agile-chronicles-1-stressful.html";
+    String outputFormat = "MOBI";
+
+
     @Test
+    @Ignore
     public void sendRequestTest(){
 
         DotEpubRequest dotEpubRequest = new DotEpubRequest();
 
-        String result = "";
+        File result = null;
         try {
-            result = dotEpubRequest.sendRequest();
+            result = dotEpubRequest.sendRequestForConversionDotEpub("<b>hola</b>", urlToConvert, outputFormat);
         } catch (Exception e) {
             e.printStackTrace();
         }
 
 
-        Assert.assertTrue(!result.isEmpty());
+        Assert.assertTrue(!result.exists());
 
     }
+
+    @Test
+    @Ignore
+    public void getHtmlContentTest(){
+
+        String urlToConvert = "http://jessewarden.com/2008/11/agile-chronicles-1-stressful.html";
+
+        DotEpubRequest dotEpubRequest = new DotEpubRequest();
+
+        String htmlToConvert = dotEpubRequest.getHtmlContent(urlToConvert);
+
+        Assert.assertTrue(!htmlToConvert.isEmpty());
+    }
+
+    public void getHtmlContentAndSendToEmail(){
+
+
+        String urlToConvert = "http://jessewarden.com/2008/11/agile-chronicles-1-stressful.html";
+
+        DotEpubRequest dotEpubRequest = new DotEpubRequest();
+
+        String htmlToConvert = dotEpubRequest.getHtmlContent(urlToConvert);
+
+
+    }
+
+    @Test
+    //@Ignore
+    public void getHtmlContentAndConvertWitDotEpubTest(){
+
+
+        DotEpubRequest dotEpubRequest = new DotEpubRequest();
+
+        String htmlToConvert = dotEpubRequest.getHtmlContent(urlToConvert);
+
+        File result = null;
+        try {
+            result = dotEpubRequest.sendRequestForConversionDotEpub(htmlToConvert, urlToConvert, outputFormat);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        Assert.assertTrue(result.exists());
+
+
+    }
+
 }
