46cdf956d47f08b8657073959eb726343ed1dd04
Cindy Marín <laslorma@gmail.com>
2016-01-11 18:06:55 -0500

    Merge remote-tracking branch 'remotes/origin/master' into issue-2-2
    # Conflicts:
    #	src/main/ionic/ebookme/www/index.html

--- src/main/ionic/ebookme/www/js/controllers.js
+++ src/main/ionic/ebookme/www/js/controllers.js
@@ -1,6 +1,18 @@
-angular.module('app.controllers', [])
-     
-.controller('ebookmeCtrl', function($scope) {
+angular.module('app.controllers', ['ngResource'])
+
+.controller('ebookmeCtrl', function($scope,EbookMeService) {
+
+    $scope.urlToConvert = {url:"",email:"",format:"HTML"};
+
+    $scope.sendUrlToConvert = function(){
+
+      EbookMeService.restConvert().sendUrl($scope.urlToConvert,function(){
+
+       alert("url successfully converted");
+      },function(er){
+
+        alert("url failure in convertion");
+      })
+    };
 
 })
- 
