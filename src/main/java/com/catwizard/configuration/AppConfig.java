10123b1fd7d65fa755103edf2d683883760c652e
poolebu <poolebu@gmail.com>
2016-01-11 19:10:51 -0500

    Added logging in the AppConfig

--- src/main/java/com/catwizard/configuration/AppConfig.java
+++ src/main/java/com/catwizard/configuration/AppConfig.java
@@ -1,6 +1,8 @@
 package com.catwizard.configuration;
 
 import com.catwizard.service.EmailService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,11 +23,16 @@ public class AppConfig {
     @Autowired
     EmailService emailService;
 
+    private final Logger log = LoggerFactory.getLogger(AppConfig.class);
+
+
     @Bean
     public EmailService loadGmailConfiguration(){
 
         emailService.setUsername(env.getProperty("email"));
         emailService.setPassword(env.getProperty("password"));
+
+        log.info("loading user and login gmail password");
 
         return emailService;
 
