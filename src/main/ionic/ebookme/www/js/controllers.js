76ea44ecd28c3c56201048279f9bba53cf93d2ef
Cindy Marín <laslorma@gmail.com>
2016-01-12 20:29:08 -0500

    agrega loading y mensajes de success y failure

--- src/main/ionic/ebookme/www/js/controllers.js
+++ src/main/ionic/ebookme/www/js/controllers.js
@@ -1,18 +1,48 @@
 angular.module('app.controllers', ['ngResource'])
 
-.controller('ebookmeCtrl', function($scope,EbookMeService) {
+  .controller('ebookmeCtrl', function($scope,EbookMeService,$ionicLoading,$timeout) {
 
     $scope.urlToConvert = {title:"",url:"",email:"",format:"HTML"};
 
     $scope.sendUrlToConvert = function(){
 
+      $scope.successMessage = undefined;
+      $scope.errorMessage = undefined;
+
+      $ionicLoading.show({
+        content: 'Loading',
+        animation: 'fade-in',
+        showBackdrop: true,
+        maxWidth: 200,
+        showDelay: 0
+      });
+
       EbookMeService.restConvert().sendUrl($scope.urlToConvert,function(){
 
-       alert("url successfully converted");
+        $ionicLoading.hide();
+        $scope.successMessage = "url successfully converted, check your email";
+        $scope.convertForm.$setPristine();
+
       },function(er){
 
-        alert("url failure in convertion");
+        $ionicLoading.hide();
+        $scope.errorMessage = "the convertion of your content failed, please try again later";
+
+
+
       })
     };
 
-})
+
+    $scope.reset=function(){
+
+      if ($scope.convertForm)
+        $scope.convertForm.$setPristine();
+
+      $scope.urlToConvert = {title:"",url:"",email:"",format:"HTML"};
+      $scope.successMessage = undefined;
+      $scope.errorMessage = undefined;
+
+    }
+
+  });
