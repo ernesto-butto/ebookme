16a30f5abb637b5759a9f86d1dacb970e021d48a
Cindy Marín <laslorma@gmail.com>
2016-02-16 15:08:41 -0430

    Merge remote-tracking branch 'remotes/origin/master' into issue-56
    # Conflicts:
    #	src/main/ionic/ebookme/www/js/controllers.js

--- src/main/ionic/ebookme/www/js/controllers.js
+++ src/main/ionic/ebookme/www/js/controllers.js
@@ -1,29 +1,55 @@
 angular.module('app.controllers', ['ngResource'])
 
-  .controller('ebookmeCtrl', function($scope,EbookMeService,$ionicLoading,$timeout,$stateParams,StorageService) {
+  .controller('EbookmeCtrl', function($scope,EbookMeService,$ionicLoading,$timeout,$stateParams,StorageService,emailFilteredFilter) {
+
+
+    $scope.emailsSaved = StorageService.get('emailsSaved');
+
+    //  $scope.urlToConvertemail = "ddd";
 
     if($stateParams.title && $stateParams.url){
-      $scope.urlToConvert = {title:$stateParams.title,url:$stateParams.url,email:"",format:"HTML",emailsSaved:StorageService.get('emailsSaved')};
+
+      $scope.urlToConvert = {title:$stateParams.title,url:$stateParams.url,email:{text: ''},format:"HTML"};
+
     }else
-      $scope.urlToConvert = {title:"",url:"",email:"",format:"HTML",emailsSaved:StorageService.get('emailsSaved')};
+
+      $scope.urlToConvert = {title:"",url:"",email:{text: ''},format:"HTML"};
 
 
     var addEmailToLocalStorage = function (email){
 
-      var emailsSaved = StorageService.get('emailsSaved');
+      //  var emailsSaved = StorageService.get('emailsSaved');
 
-      if(emailsSaved.indexOf(email)== -1){
+      var flag =false;
+      for(var i=0; i < $scope.emailsSaved.length;i++){
 
-        emailsSaved.push(email);
+        if($scope.emailsSaved[i].text.indexOf(email.text)> -1){
 
+          flag=true;
+
+        }
       }
 
-      StorageService.set('emailsSaved',emailsSaved);
+      if(!flag){
+        $scope.emailsSaved.push(email);
+      }
+
+
+      StorageService.set('emailsSaved',$scope.emailsSaved);
 
     };
 
+    var resetForm=function(form){
 
-    $scope.sendUrlToConvert = function(){
+      if (form)
+        form.$setPristine();
+
+      $scope.urlToConvert = {title:"",url:"",email:"",format:"HTML"};
+
+
+    };
+
+    $scope.sendUrlToConvert = function(form){
 
       $scope.successMessage = undefined;
       $scope.errorMessage = undefined;
@@ -36,43 +62,74 @@ angular.module('app.controllers', ['ngResource'])
         showDelay: 0
       });
 
-      addEmailToLocalStorage($scope.urlToConvert.email);
+
 
       EbookMeService.restConvert().sendUrl($scope.urlToConvert,function(){
 
 
         $ionicLoading.hide();
         $scope.successMessage = "Url successfully converted, check your email";
-        $scope.convertForm.$setPristine();
-
+        addEmailToLocalStorage($scope.urlToConvert.email);
+        resetForm(form);
 
       },function(er){
 
         $ionicLoading.hide();
         $scope.errorMessage = "The convertion of your content failed, please try again later";
 
-      })
+      });
+
+
+      /* $timeout(function() {
+
+       $ionicLoading.hide();
+       $scope.successMessage = "Url successfully converted, check your email";
+       addEmailToLocalStorage($scope.urlToConvert.email);
+       resetForm(form);
+
+
+       },2000); */
     };
 
 
-    $scope.reset=function(){
 
-      if ($scope.convertForm)
-        $scope.convertForm.$setPristine();
 
-      $scope.urlToConvert = {title:"",url:"",email:"",format:"HTML"};
+    $scope.resetAll = function (form){
+      resetForm(form);
       $scope.successMessage = undefined;
       $scope.errorMessage = undefined;
-
     };
+
+
 
 
     $scope.setEmail = function (email){
       $scope.urlToConvert.email=email;
+
+    };
+
+    $scope.search = function(){
+
+      $scope.urlToConvert.emailsSaved =  emailFilteredFilter($scope.urlToConvert.email,$scope.urlToConvert.emailsSaved)
+
     };
 
 
+  })
 
+  .filter('emailFiltered', function() {
+    return function (email,emailsList) {
+      return function (email,emailsList) {
+        var filteredList = [];
+        for(var i = 0 ; i < emailsList.length ; i++){
+          if(emailsList[i].indexOf(email)>= 0){
+            filteredList.push(emailsList[i]);
+          }
+        }
+
+
+      };
+    }
   })
 
   .controller('SuggestionsCtrl', function($scope,$state) {
