f98c4a4c534b420a965c13e7e7893460970a77c1
Cindy Marín <laslorma@gmail.com>
2016-02-17 09:19:47 -0430

    agrega validacion de email en controller y ng-change del input email, ya que al usar ng-pattern no se hacia el binding al model lo que causaba que no se comportara correctamente el autocomplete

--- src/main/ionic/ebookme/www/js/controllers.js
+++ src/main/ionic/ebookme/www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('app.controllers', ['ngResource'])
 
-  .controller('EbookmeCtrl', function($scope,EbookMeService,$ionicLoading,$timeout,$stateParams,StorageService,emailFilteredFilter) {
+  .controller('EbookmeCtrl', function($scope,EbookMeService,$ionicLoading,$timeout,$stateParams,StorageService) {
 
 
     $scope.emailsSaved = StorageService.get('emailsSaved');
@@ -16,25 +16,27 @@ angular.module('app.controllers', ['ngResource'])
       $scope.urlToConvert = {title:"",url:"",email:'',format:"HTML"};
 
 
-    var addEmailToLocalStorage = function (email){
-
-
-
-      var flag =false;
+    var checkIfEmailExist = function(email){
 
       for(var i=0; i < $scope.emailsSaved.length;i++){
 
         if($scope.emailsSaved[i].indexOf(email)> -1){
 
-          flag=true;
+          return true;
 
         }
       }
 
-      if(!flag){
+      return false;
+
+    };
+
+    var addEmailToLocalStorage = function (email){
+
+
+      if(!checkIfEmailExist(email)){
         $scope.emailsSaved.push(email);
       }
-
 
       StorageService.set('emailsSaved',$scope.emailsSaved);
 
@@ -98,32 +100,25 @@ angular.module('app.controllers', ['ngResource'])
 
     $scope.setEmail = function (email){
       $scope.urlToConvert.email=email;
-
-    };
-
-    $scope.search = function(){
-
-      $scope.urlToConvert.emailsSaved =  emailFilteredFilter($scope.urlToConvert.email,$scope.urlToConvert.emailsSaved)
+      $scope.validateEmail();
 
     };
 
 
+
+
+    $scope.validateEmail = function(){
+
+      var EMAIL_REGEXP = /^[_a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
+
+      $scope.validEmail =   EMAIL_REGEXP.test($scope.urlToConvert.email);
+
+
+    };
+
   })
 
-  .filter('emailFiltered', function() {
-    return function (email,emailsList) {
-      return function (email,emailsList) {
-        var filteredList = [];
-        for(var i = 0 ; i < emailsList.length ; i++){
-          if(emailsList[i].indexOf(email)>= 0){
-            filteredList.push(emailsList[i]);
-          }
-        }
 
-
-      };
-    }
-  })
 
   .controller('SuggestionsCtrl', function($scope,$state) {
 
